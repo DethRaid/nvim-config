fe7fda23e8cfc298dca9b386e393791112884710
Sarah Dubois <sarahduboisengineer@gmail.com>
2026-01-24 11:30:14 -0800

    only autoformat on save

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -753,7 +753,6 @@ local plugin_specs = {
         vim.cmd([[
             let g:clang_format#detect_style_file=1
             let g:clang_format#auto_format=1
-            let g:clang_format#auto_format_on_insert_leave=1
         ]])
     end
   }
